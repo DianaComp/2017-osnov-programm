e79d5d47242b973f24a23e56dd67dc2a55d067b0
Diana <di.senkina@yandex.ru>
2017-10-16 16:43:17 +0200

    add POS tagger

--- homework/POS.py
+++ homework/POS.py
@@ -5,6 +5,12 @@ vocab =  sys.stdin.readlines()
 table = {}
 
 fd = open("POS_vocab1.txt")
+for line in fd.readlines():
+	line = line.strip("\n")
+	line = line.split("\t")
+	inn = line[0]
+	out = line[1]
+	table[inn] = out
 
 for b in vocab:
 	b = b.strip()
@@ -12,8 +18,7 @@ for b in vocab:
 		print(b)
 		continue
 	line = b.split("\t")
-	pos = line[1]
-	for y in table:
-		form = pos.replace(y, table[y])
-	line[3] = pos
+	surf = line[1]
+	if surf in table:
+		line[3] = table[surf]
 	print("\t".join(line))
